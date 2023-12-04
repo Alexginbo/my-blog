051f61e9079a043b4835bf765e8b1a38611bf6c9
alexandre.giner <alexandre.giner@inetum.com>
2023-12-04 15:49:27 +0100

    Category creation exists controlled

--- blog-api/controllers/CategoryController.js
+++ blog-api/controllers/CategoryController.js
@@ -1,32 +1,33 @@
 const Category = require('../models/category')
 
-function createCategory (req, res) {
+async function createCategory (req, res) {
     let categoryToSave = new Category()
-    if (req.body.parentCategory && req.body.parentCategory !== "") {
-        Category.findOne({ 'categoryName': req.body.parentCategory}).then(
-            (foundCategory, err) => {
-                if (err || !foundCategory) {
-                    return res.status(404).send({
-                        msg: 'Parent Category Not Found',
-                        err: err
-                    })
-                } else {
-                    categoryToSave.parentCategory = foundCategory
-                    categoryToSave.categoryName = req.body.categoryName
-                    categoryToSave = categoryToSave.save()
-                    return res.status(200).send({
-                        categoryToSave: categoryToSave
-                    })
-                }
-            }
-        )
-    } else {
-                    categoryToSave.categoryName = req.body.categoryName
-                    categoryToSave.save()
-                    return res.status(200).send({
-                        categoryToSave: categoryToSave
-                    })
+    if (!req.body.categoryName) {
+        return res.status(400).send({
+            msg: 'Please, enter a category name'
+        })
     }
+    if (req.body.parentCategory && req.body.parentCategory !== "") {
+        categoryToSave.parentCategory = await Category.findOne({ 'categoryName': req.body.parentCategory})
+        if (!categoryToSave.parentCategory) {
+            return res.status(404).send({
+                msg: 'Parent category not found'
+            })
+        }
+    }
+    categoryToSave.categoryName = await Category.findOne({ 'categoryName': req.body.categoryName})
+    if (categoryToSave.categoryName) {
+        return res.status(400).send({
+            msg: 'This category already exists'
+        })
+    }
+    categoryToSave.save().then((savedCategory) => {
+        if (savedCategory) {
+            return res.status(200).send({
+                categoryToSave: categoryToSave
+            })
+        }
+    })
     
 }
 
